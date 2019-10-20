d7893c51b020d5188386cfc2a45ae770491c6fcf
Mattia <sconqua@gmail.com>
2019-10-20 20:13:22 +0200

    ✨ Firebase setup
     #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from '../environments/environment';
+import { AngularFireDatabase } from '@angular/fire/database';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
-  providers: [],
+  providers: [
+    AngularFireDatabase
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBLJlRJAKTZ3wnZKEEEzVDSTajJecp20HU",
+    authDomain: "shared-shopping-list-8aca2.firebaseapp.com",
+    databaseURL: "https://shared-shopping-list-8aca2.firebaseio.com",
+    projectId: "shared-shopping-list-8aca2",
+    storageBucket: "shared-shopping-list-8aca2.appspot.com",
+    messagingSenderId: "431938162880",
+    appId: "1:431938162880:web:3a3f5020c97a8505e01d25",
+    measurementId: "G-MZ3BHS9WH1"
+  }
 };
 
 /*
