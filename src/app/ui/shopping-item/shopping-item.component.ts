cd3c7bf8cb9cd8998c9e7f1b48f2fcb0f41deacd
Mattia <sconqua@gmail.com>
2019-10-23 04:25:54 +0200

    💄 Animate list display
     #25

--- src/app/ui/shopping-item/shopping-item.component.ts
+++ src/app/ui/shopping-item/shopping-item.component.ts
@@ -1,6 +1,16 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
+  animations: [
+    trigger('items', [
+      transition(':enter', [
+        style({ transform: 'scale(0.5)', opacity: 0 }),  // initial
+        animate('1s cubic-bezier(.8, -0.6, 0.2, 1.5)',
+          style({ transform: 'scale(1)', opacity: 1 }))  // final
+      ])
+    ])
+  ],
   selector: 'app-shopping-item',
   templateUrl: './shopping-item.component.html',
   styleUrls: ['./shopping-item.component.scss']
