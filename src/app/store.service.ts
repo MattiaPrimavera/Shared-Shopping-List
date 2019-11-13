f86c323d41335d2f51f6a7ba19fde39ce4dd6564
Mattia Primavera <sconqua@gmail.com>
2019-11-13 03:13:50 +0100

    ✨ Simple store implementation

---
+++ src/app/store.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+interface State {
+  isAuth: boolean,
+  uid: string
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+  state: State
+
+  constructor() {}
+
+  getState(): State {
+    return this.state
+  }
+
+  setState(state: State) {
+    this.state = state
+  }
+}
