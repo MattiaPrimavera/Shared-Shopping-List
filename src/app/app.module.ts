90dafe98beba8b60c5f7cd6fe29df42c026c37ff
Mattia Primavera <sconqua@gmail.com>
2019-11-14 19:45:35 +0100

    💄 Add expansion panel to lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatExpansionModule } from '@angular/material/expansion'
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -61,6 +62,7 @@ import { ToDoOnlyPipe } from './pipes/to-do-only.pipe';
     MatIconModule,
     MatInputModule,
     MatButtonModule,
+    MatExpansionModule,
     MatListModule,
     MatMenuModule,
     MatBottomSheetModule,
