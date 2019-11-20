b1dad54f6788965fae76ff4b1e6be4fc0d2a51bb
sconqua <sconqua@gmail.com>
2019-11-20 02:08:45 +0100

    💄 Chat: Automatically scroll bottom to last message
      #55

--- src/app/ui/components/chat-widget/chat-widget.component.ts
+++ src/app/ui/components/chat-widget/chat-widget.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { ChatMessage } from 'src/app/services/database/chat/chat.service';
 
 @Component({
@@ -9,6 +9,10 @@ import { ChatMessage } from 'src/app/services/database/chat/chat.service';
 export class ChatWidgetComponent implements OnInit {
   @Input() messages: ChatMessage[];
   @Input('myUid') uid: string;
+  @ViewChild('scrollFrame', {static: false}) scrollFrame: ElementRef;
+  @ViewChildren('item') itemElements: QueryList<any>;
+  private scrollContainer: any;
+  isNearBottom: boolean;
 
   constructor() { }
 
@@ -16,5 +20,36 @@ export class ChatWidgetComponent implements OnInit {
 
   isMyMessage(message) {
     return message.uid === this.uid;
+  }
+
+  ngAfterViewInit() {
+    this.scrollContainer = this.scrollFrame.nativeElement;
+    this.itemElements.changes.subscribe(_ => this.onItemElementsChanged());
+    this.scrollToBottom();
+  }
+
+  private onItemElementsChanged(): void {
+    if (true) {
+      this.scrollToBottom();
+    }
+  }
+
+  private scrollToBottom(): void {
+    this.scrollContainer.scroll({
+      top: this.scrollContainer.height,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  scrolled(event: any): void {
+    this.isNearBottom = this.isUserNearBottom();
+  }
+
+  private isUserNearBottom(): boolean {
+    const threshold = 150;
+    const position = this.scrollContainer.scrollTop + this.scrollContainer.offsetHeight;
+    const height = this.scrollContainer.scrollHeight;
+    return position > height - threshold;
   }
 }
